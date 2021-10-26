dbc8ad66f277dc4a7947ba7618a4e2ef83e2d9ee
Radoslaw Czerniawski <radoslaw.czerniawski@o2.pl>
2021-10-26 18:46:49 +0200

    Finished the video player exercise and renamed its dir

--- js/videoplayer/main.js
+++ js/videoplayer/main.js
@@ -1,11 +1,108 @@
-function createPlayer(container) {
-    const video = container.querySelector("video");
-    const timeline = container.querySelector("[data-js-timeline]");
-    const play = container.querySelector("[data-js-play]");
-    const time = container.querySelector("[data-js-time]");
-    const duration = container.querySelector("[data-js-duration]");
-    const progress = container.querySelector("[data-js-progress]");
+(function () {
+    const button = document.querySelector(".button");
 
-}
+    function renderVideoPlayer() {
+        const player = `
+        <div class="edu-player__video">
+            <span class="icon">
+                <i class="fas fa-play" data-js-play2></i>
+            </span>
 
-createPlayer(document.querySelector("[data-js-player]"));
+            <video></video>
+        </div>
+
+        <div class="edu-player__timeline" data-js-timeline>
+            <div class="edu-player__controls">
+                <span class="icon">
+                    <i class="fas fa-play" data-js-play></i>
+                </span>
+            </div>
+            <div class="edu-player__controls">
+                <span data-js-time>00:00</span>
+                <span data-js-duration>12:53</span>
+            </div>
+            <div class="edu-player__progress" data-js-progress></div>
+        </div>
+        `;
+
+        const div = document.createElement("div");
+        div.innerHTML = player;
+        document.querySelector(".container").append(div);
+        div.classList.add("edu-player");
+        div.setAttribute("data-js-player","");
+        button.remove();
+    };
+
+    function createPlayer(container, src) {
+        const video = container.querySelector("video");
+        const timeline = container.querySelector("[data-js-timeline]");
+        const play = container.querySelector("[data-js-play]");
+        const play2 = container.querySelector("[data-js-play2]");
+        const time = container.querySelector("[data-js-time]");
+        const duration = container.querySelector("[data-js-duration]");
+        const progress = container.querySelector("[data-js-progress]");
+
+        function secondsToTime(seconds) {
+            return [seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60]
+                .map((v) => (v < 10 ? `0${parseInt(v, 10)}` : parseInt(v, 10)))
+                .filter((i, j) => i !== "00" || j > 0)
+                .join(":");
+        }
+
+        function togglePlay(e) {
+            e.stopPropagation();
+
+            if(video.paused) {
+                video.play();
+                play.classList.remove("fa-play");
+                play.classList.add("fa-pause");
+                play2.style.visibility = "hidden";
+            } else {
+                video.pause();
+                play.classList.remove("fa-pause");
+                play.classList.add("fa-play");
+                play2.style.visibility = "visible";
+            }
+        }
+
+        function updateDuration(e) {
+            duration.textContent = secondsToTime(e.currentTarget.duration);
+        }
+
+        function updateTime(e) {
+            time.textContent = secondsToTime(e.currentTarget.currentTime);
+        }
+
+        function updateProgress(e) {
+            progress.style.width = `${e.currentTarget.currentTime / e.currentTarget.duration * 100}%`;
+        }
+
+        function setTime(e) {
+            const pos = e.currentTarget.getBoundingClientRect();
+            const left = e.pageX - pos.left;
+            const percentage = left / pos.width;
+
+            video.currentTime = video.duration * percentage;
+        }
+
+        play.addEventListener("click", togglePlay);
+        play2.addEventListener("click", togglePlay);
+        timeline.addEventListener("click", setTime);
+
+        video.addEventListener("click", togglePlay);
+        video.addEventListener("timeupdate", updateProgress);
+        video.addEventListener("durationchange", updateDuration);
+        video.addEventListener("timeupdate", updateTime);
+
+        video.setAttribute("src", src);
+    }
+
+    button.addEventListener("click", () => {
+        renderVideoPlayer();
+        createPlayer(
+            document.querySelector("[data-js-player]"),
+            "https://space.eduweb.pl/files/assets/video.mp4"
+        );
+    });
+})();
+
